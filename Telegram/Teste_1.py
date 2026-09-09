9900ad5d141f9aeb822f6f34269425531258cb3e
Lamoni Leal Pereira <lamoni.s.p@gmail.com>
2026-09-09 18:30:24 -0300

    Update objetos e funções & Log de Execução

--- Telegram/Teste_1.py
+++ Telegram/Teste_1.py
@@ -1,48 +1,74 @@
-from telethon.tl.functions.channels import JoinChannelRequest
-from telethon import TelegramClient
-from google import genai
-from google.genai import types
-from datetime import datetime
-from pydantic import BaseModel
-from dotenv import load_dotenv, find_dotenv
-import os
+
+"""
+**__**
+Projeto: Millie App 1.0
+Equipe: **Desenvolvedores Universitários PUC - MG**
+
+Gerente & Dev Back-end: Mateus Coelho Pires
+Product Owner & Dev Back-end: Lamôni Leal Pereira
+Dev Front-end: Arthur Utsch
+Designer e Sup Front-end: Luiza Mariano
+Analista QA e Administradora: Bruna Santiago
+**__**
+Arquivo: Teste_1.py
+Descrição:
+ 
+EMPRESA:       [Millie Tecnologies]
+SISTEMA:       [Millie App - Desenvolvimento de Software]
+MÓDULO:        []
+ARQUIVO:       [Teste_1.py]
+--------------------------------------------------------------------------
+DESCRIÇÃO:  [Este arquivo contém as funções principais da integração]
+ [da API Telegram com API Gemini, tem como objetivo gerar os objetos estruturados]
+ [em JSON necesários para análise de dados, incluindo valores de cotação de milhas]
+ [transferências, compras e adesões a clubes bonificadas.]
+--------------------------------------------------------------------------
+AUTOR:         [Lamoni Leal Pereira] <lamoni.pereira@sga.pucminas.br>
+DATA CRIAÇÃO:  [09/09/2026]
+--------------------------------------------------------------------------
+DIREITOS:      (c) Copyright [2026] - [Millie Tecnologies]. 
+               Todos os direitos reservados. Este código é confidencial e 
+               propriedade interna da empresa.
+==========================================================================
+----------  --------------  -------  -------------------------------------
+"""
+
 import asyncio
-
-
+import os
+from dotenv import load_dotenv, find_dotenv
+from pydantic import BaseModel
+from datetime import datetime
+from google.genai import types
+from google import genai
+from telethon import TelegramClient
+from telethon.tl.functions.channels import JoinChannelRequest
+import Teste_Objetos
+import telethon
+print('Abrindo Bibliotecas...')
+print('Carregando variáveis de ambiente...')
+# Carregando variáveis de ambiente do arquivo .env
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
-
+# instanciando o cliente do Telegram e do Gemini
 TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
 TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
-GEMINI_API_HASH = os.getenv("GEMINI_API_HASH")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+print('Variáveis de ambiente carregadas com sucesso.\n'+'*'*40)
 
+
+print('Estabelecendo chaves de acesso...')
+# Aplicando Chaves de API do Telegram
 Telegram_Client = TelegramClient(
     "session_name", TELEGRAM_API_ID, TELEGRAM_API_HASH)
 limit = 1
-Gemini_Client = genai.Client(
-    api_key='GEMINI_API_HASH')
-
-
-class Cotacao(BaseModel):
-    Plataforma: str
-    Agencia: str
-    Media: float
-    Data: datetime
-    Monte: int
-
-    def Print_Cota(self):
-        print('--' * 40)
-        print('Plataforma:', self.Plataforma)
-        print('Agencia:', self.Agencia)
-        print('Valor Médio Cotação: R$', self.Media)
-        print('Montante Acumulado:', self.Monte)
-        print(
-            'Última Atualização:',
-            self.Data.strftime("%d/%m/%Y %H:%M")
-        )
+# Aplicando Chave de API do Gemini
+Gemini_Client = genai.Client(api_key=GEMINI_API_KEY)
+print('Chaves de acesso estabelecidas.\n'+'*'*40)
+# Função para entrar em um canal do Telegram
 
 
 async def Entrar_Canal(Telegram_Client, Canal):
+    print('Entrando no Canal: ', Canal)
     try:
         await Telegram_Client(JoinChannelRequest(Canal))
         print(f"Sucesso em entrar no Canal {Canal}")
@@ -50,26 +76,57 @@ async def Entrar_Canal(Telegram_Client, Canal):
         print(f"falha ao entrar no Canal {Canal}")
         # print(f"Erro: {e}")
 
+# Função de busca por mensagens em um canal do Telegram com base em uma palavra-chave
+
 
 async def Procura_mensagem(Telegram_Client, Canal, limit, Palavra):
     print('Procurando mensagem...')
     async for message in Telegram_Client.iter_messages(Canal, limit, search=Palavra):
+        print(f"Mensagem encontrada.\n")
         if message.text:
             return message
 
+# Funçao de Prompt do Gemini para Cotação de Milhas
+# Resposta do Gemini estruturada em JSON
 
-def Prompt_Gemini(Prompt: str):
+
+def Prompt_Gemini_Cotacao(Prompt: str):
+    print('Enviando Prompt para o Gemini...\nAguarde a resposta...')
     try:
         response = Gemini_Client.models.generate_content(
             model='gemini-3.5-flash',
             contents=Prompt,
             config={
                 "response_mime_type": "application/json",
-                "response_schema": Cotacao
+                "response_schema": Teste_Objetos.Cotacao
             }
-
         )
 
+        print('Resposta recebida do Gemini.\n')
+        # Retornando a resposta do Gemini como um objeto Cotacao
+        return response.parsed
+    except Exception as e:
+        print("Erro na conexão com o Gemini:")
+        print(e)
+        return None
+
+# Função de Prompt do Gemini para Transferências Bonificadas de Milhas
+# Resposta do Gemini estruturada em JSON
+
+
+def Prompt_Gemini_Transferencia(Prompt: str):
+    print('Enviando Prompt para o Gemini...\nAguarde a resposta...')
+    try:
+        response = Gemini_Client.models.generate_content(
+            model='gemini-3.5-flash',
+            contents=Prompt,
+            config={
+                "response_mime_type": "application/json",
+                "response_schema": Teste_Objetos.Transferencia_Bonificada
+            }
+        )
+        print('Resposta recebida do Gemini.\n')
+        # Retornando a resposta do Gemini como um objeto Transferencia_Bonificada
         return response.parsed
     except Exception as e:
         print("Erro na conexão com o Gemini:")
@@ -78,43 +135,64 @@ def Prompt_Gemini(Prompt: str):
 
 
 async def main():
+    # Instanciando os Link dos canais do Telegram
     Canal_Cota = 'https://t.me/cotacoes'
-    Opcao = ' '
+    Canal_Pronto_voar = 'https://t.me/canalpontospravoar'
+    Opcao = ' '  # Instanciando a variável de opção do menu
     while Opcao != '0':
-
+        # Menu de opções do Teste da API do Telegram
         print('Menu Teste Telegram API')
         print('-'*40)
-        print('1 - Entrar no canal de Cotações')
-        print('2 - Pesquisar no Canal de cotação uma mensagem de milhas')
-        print('3 - Perguntar ao Gemini')
+        print('1 - Entrar em todos os canais')
+        print('2 - Pesquisar Cotação de Milhas')
+        print('3 - Pesquisar Transferência Bonificada')
+        print('4 - Perguntar ao Gemini sobre Cotação de Milhas')
+        print('5 - Perguntar ao Gemini sobre Transferência Bonificada')
         print('0 - Sair')
         Opcao = input("Digite a Opção: ")
+
+        # Executando a opção escolhida
         match Opcao:
             case '1':
                 # Entrando No Canal de Cotações de Milhas
                 await Entrar_Canal(Telegram_Client, Canal_Cota)
+                await Entrar_Canal(Telegram_Client, Canal_Pronto_voar)
             case '2':
                 # Coletando Última Mensagem de Cotação de milhas
-                Palavra_Procura = input("Digite a Palavra a se pesquisar: ")
-                mensagem = await Procura_mensagem(Telegram_Client, Canal_Cota, limit, Palavra_Procura)
-                if mensagem is not None:
-                    print(mensagem.text)
+                Palavra_Procura = 'Tendencias'
+                mensagem_Cota = await Procura_mensagem(Telegram_Client, Canal_Cota, limit, Palavra_Procura)
+                # Imprimindo a mensagem encontrada ou informando que não foi encontrada
+                if mensagem_Cota is not None:
+                    print(mensagem_Cota.text)
                 else:
                     print('Mensagem Não Encontrada')
             case '3':
-                # Prompt = ("atualização de hoje venda de milhas na balcao de milhas,"" cotação da smiles é de R$15,90, 180000 milhas acumiladas")
-                Prompt = mensagem.text
-                Resposta = Prompt_Gemini(Prompt)
-
+                # Coletando Última Mensagem de Transferência Bonificada
+                Palavra_Procura = 'Transferência bonificada'
+                mensagem_Transf = await Procura_mensagem(Telegram_Client, Canal_Pronto_voar, limit, Palavra_Procura)
+                # Imprimindo a mensagem encontrada ou informando que não foi encontrada
+                if mensagem_Transf is not None:
+                    print(mensagem_Transf.text)
+                else:
+                    print('Mensagem Não Encontrada')
+            case '4':
+                # Coletando Última Mensagem de Cotação de milhas
+                Prompt = mensagem_Cota.text
+                Resposta = Prompt_Gemini_Cotacao(Prompt)
+                # Imprimindo a resposta do Gemini ou informando que não foi encontrada
                 if Resposta is not None:
                     print("\nResposta do Gemini:")
                     print("-" * 40)
                     Resposta.Print_Cota()
-                # if cota is not None:
-                #   cota.Print_Cota()
-                # else:
-                #   print("Não foi possível obter a cotação.")
-
+            case '5':
+                # Coletando Última Mensagem de Transferência Bonificada
+                Prompt = mensagem_Transf.text
+                Resposta = Prompt_Gemini_Transferencia(Prompt)
+                # Imprimindo a resposta do Gemini ou informando que não foi encontrada
+                if Resposta is not None:
+                    print("\nResposta do Gemini:")
+                    print("-" * 40)
+                    Resposta.Print_Transferencia()
 
 with Telegram_Client:
     Telegram_Client.loop.run_until_complete(main())
--- Telegram/Teste_Objetos.py
+++ Telegram/Teste_Objetos.py
@@ -1,23 +1,110 @@
+"""
+**__**
+Projeto: Millie App 1.0
+Equipe: **Desenvolvedores Universitários PUC - MG**
+
+Gerente & Dev Back-end: Mateus Coelho Pires
+Product Owner & Dev Back-end: Lamôni Leal Pereira
+Dev Front-end: Arthur Utsch
+Designer e Sup Front-end: Luiza Mariano
+Analista QA e Administradora: Bruna Santiago
+**__**
+Arquivo: Teste_Objetos.py
+Descrição:
+ 
+EMPRESA:       [Millie Tecnologies]
+SISTEMA:       [Millie App - Desenvolvimento de Software]
+MÓDULO:        []
+ARQUIVO:       [Teste_Objetos.py]
+--------------------------------------------------------------------------
+DESCRIÇÃO:  [Este arquivo contém as classes de objetos que representam as] 
+            [respostas estruturadas em JSON do Gemini para as estruturas de dados]
+            [de Cotação de Milhas, Transferências,, Compras, e Adesões Bonificadas]
+--------------------------------------------------------------------------
+AUTOR:         [Lamoni Leal Pereira] <lamoni.pereira@sga.pucminas.br>
+DATA CRIAÇÃO:  [09/09/2026]
+--------------------------------------------------------------------------
+DIREITOS:      (c) Copyright [2026] - [Millie Tecnologies]. 
+               Todos os direitos reservados. Este código é confidencial e 
+               propriedade interna da empresa.
+==========================================================================
+----------  --------------  -------  -------------------------------------
+"""
+
 from datetime import datetime
+
+# Classe de cotação de milhas
 
 
 class Cotacao:
-    def __init__(self, Plataforma: str, Agencia: str, Media: float, Data, Monte: str):
+    def __init__(self, Plataforma: str, Agencia: str, Media: float, Data_Vencimento, Monte: str):
         self.Plataforma = Plataforma
         self.Agencia = Agencia
         self.Media = Media
         self.Monte = Monte
-        self.Data = Data
+        self.Data_Vencimento = Data_Vencimento
 
     def Print_Cota(self):
         print('--'*40 + '\nPlataforma: ', self.Plataforma)
         print('Agencia: ', self.Agencia)
         print('Valor Médio Cotação: R$', self.Media)
         print('Montante Acumulado: ', self.Monte)
-        print('Última Atualização: ', self.Data.strftime("%d/%m/%Y %H:%M"))
+        print('Última Atualização: ',
+              self.Data_Vencimento.strftime("%d/%m/%Y %H:%M"))
+
+# Classe de transferencia bonificada de milhas
 
 
-Smiles_Data = datetime(2026, 8, 28, 10, 30)
-Smiles_Cota = Cotacao('Balcão de Milhas', 'Smiles', 15.0, Smiles_Data, 500000)
+class Transferencia_Bonificada:
+    def __init__(self, Origem: str, Destino: str, Porcent_High: float, Porcent_Low: float, Data_Vencimento, link: str):
+        self.Origem = Origem
+        self.Destino = Destino
+        self.Porcent_High = Porcent_High
+        self.Porcent_Low = Porcent_Low
+        self.Data_Vencimento = Data_Vencimento
+        self.link = link
 
-Smiles_Cota.Print_Cota()
+    def Print_Transf(self):
+        print('--'*40 + '\nOrigem: ', self.Origem)
+        print('Destino: ', self.Destino)
+        print('Porcentagem Alta: ', self.Porcent_High)
+        print('Porcentagem Baixa: ', self.Porcent_Low)
+        print('Última Atualização: ',
+              self.Data_Vencimento.strftime("%d/%m/%Y %H:%M"))
+        print('Informações da Promoção: ', self.link)
+
+
+# Classe de compra bonificada de milhas
+
+class Compra_Bonificada:
+    def __init__(self, Agencia: str, Porcent_High: float, Porcent_Low: float, Data_Vencimento, link: str):
+        self.Agencia = Agencia
+        self.Porcent_High = Porcent_High
+        self.Porcent_Low = Porcent_Low
+        self.Data_Vencimento = Data_Vencimento
+        self.link = link
+
+    def Print_Compra(self):
+        print('--'*40 + '\nAgência / Plataforma: ', self.Agencia)
+        print('Porcentagem Alta: ', self.Porcent_High)
+        print('Porcentagem Baixa: ', self.Porcent_Low)
+        print('Última Atualização: ',
+              self.Data_Vencimento.strftime("%d/%m/%Y %H:%M"))
+        print('Informações da Promoção: ', self.link)
+
+
+# Classe de adesão bonificada à uma agencia ou plataforma
+
+class Adesao_Bonificada:
+    def __init__(self, Agencia: str, Milhas: int, Data_Vencimento, link: str):
+        self.Agencia = Agencia
+        self.Milhas = Milhas
+        self.Data_Vencimento = Data_Vencimento
+        self.link = link
+
+    def Print_Adesao(self):
+        print('--'*40 + '\nAgência / Plataforma: ', self.Agencia)
+        print('Milhas Bonificadas: ', self.Milhas)
+        print('Última Atualização: ',
+              self.Data_Vencimento.strftime("%d/%m/%Y %H:%M"))
+        print('Informações da Promoção: ', self.link)
